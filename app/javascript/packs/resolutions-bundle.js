f8bba45ae9ab2c5d7074924ac7aa286d0e05b021
Jake <jdeekhof@gmail.com>
2024-03-21 23:13:30 -0400

    get rid of boilerplate react with actual names

--- app/javascript/packs/resolutions-bundle.js
+++ app/javascript/packs/resolutions-bundle.js
@@ -1,8 +1,8 @@
 import ReactOnRails from 'react-on-rails';
 
-import GoalsHome from '../bundles/Resolutions/components/GoalsHome';
+import GoalsHome from '../bundles/./Resolutions/components/GoalsHome';
 
 // This is how react_on_rails can see the GoalsHome in the browser.
 ReactOnRails.register({
-    GoalsHome,
+  GoalsHome,
 });
