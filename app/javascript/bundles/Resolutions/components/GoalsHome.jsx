58e66c602cfbb1e7edf429bdc66dc95040c42b52
Jake <jdeekhof@gmail.com>
2024-03-22 18:24:40 -0400

    first draft of create goal form

--- app/javascript/bundles/Resolutions/components/GoalsHome.jsx
+++ app/javascript/bundles/Resolutions/components/GoalsHome.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import CreateGoalForm from "./CreateGoalForm";
+import GoalsList from "./GoalsList";
 
-const humanize = (str) => {
-  return str.replace(/_/g," ")
-}
+
 const GoalsHome = (props) => {
   const queryClient = new QueryClient()
   const [goals, setGoals] = useState(Object.values(props))
@@ -13,19 +13,13 @@ const GoalsHome = (props) => {
       <div className={'lg:flex gap-3 mx-auto'}>
         <div className={'p-6 m-2 mx-auto bg-white rounded-xl shadow-lg shrink-0 basis-1/3'}>
           <h1 className={'text-3xl mb-3'}> Current Goals </h1>
-          <hr/>
-          {goals.length > 0 ? goals.map((goal) => (
-            <div>
-              <h2 className={'text-xl mt-2'}>{goal.title}</h2>
-              <p>SMART goal: Achieve {humanize(goal.comparator)} {goal.target_value} {goal.target_metric} {goal.interval}</p>
-              <p> Most recent stat: 2000 steps on 3/14</p>
-              <p> progress bar</p>
-            </div>
-          )): <h2 className={'text-xl mt-2 text-center '}> No Goals Yet</h2>}
+          <hr />
+          {goals.length > 0 ? <GoalsList goals={goals}/>: <h2 className={'text-xl mt-2 text-center '}>No Goals Yet</h2>}
         </div>
         <div className={'p-6 m-2 mx-auto bg-white rounded-xl shadow-lg shrink-0 basis-1/3'}>
           <h1 className={'text-3xl mb-3'}> Create New Goal </h1>
           <hr/>
+          <CreateGoalForm />
         </div>
         <div className={'p-6 m-2 mx-auto bg-white rounded-xl shadow-lg shrink-0 basis-1/3'}>
           <h1 className={'text-3xl mb-3'}> Log New Stat </h1>
---
+++ app/javascript/bundles/Resolutions/components/GoalsList.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import GoalsHome from "./GoalsHome";
+
+const humanize = (str) => {
+  return str.replace(/_/g," ")
+}
+const GoalsList = ({goals}) => {
+  return(
+    goals.map((goal) => (
+      <div className={'p-1 border rounded-lg w-full mt-2'}>
+        <h2 className={'text-2xl'}>{goal.title}</h2>
+        <h3 className={'text-xl'}> SMART goal: </h3>
+        <p>Achieve {humanize(goal.comparator)} {goal.target_value} {goal.target_metric} {goal.interval}</p>
+        <h3 className={'text-xl'}> Most recent stat: </h3>
+        <p> 2000 steps on 3/14</p>
+        <h3 className={'text-xl'}> Progress </h3>
+        <progress className={'w-full'} value={0.5}/>
+      </div>
+    ))
+  )
+}
+
+GoalsList.propTypes = {
+  goals: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      deadline: PropTypes.string,
+      comparator: PropTypes.string.isRequired,
+      target_value: PropTypes.string.isRequired,
+      target_metric: PropTypes.number.isRequired,
+      interval: PropTypes.string.isRequired,
+      capture_frequency: PropTypes.string.isRequired,
+      created_at: PropTypes.string.isRequired,
+      updated_at: PropTypes.string.isRequired
+    })
+  )
+}
+
+export default GoalsList
