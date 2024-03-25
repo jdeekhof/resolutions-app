82f98617878e55ef974bdb799e8605d95687903b
Jake <jdeekhof@gmail.com>
2024-03-25 17:24:14 -0400

    begin integrating stats

--- app/javascript/bundles/Resolutions/components/GoalsHome.jsx
+++ app/javascript/bundles/Resolutions/components/GoalsHome.jsx
@@ -1,12 +1,13 @@
-import PropTypes from "prop-types";
+import PropTypes, {checkPropTypes} from "prop-types";
 import React, {useEffect, useState} from "react";
 import CreateGoalForm from "./CreateGoalForm";
 import GoalsList from "./GoalsList";
 import {Create, Index} from "../Api/Goals";
 
-const GoalsHome = (props) => {
-  const [goals, setGoals] = useState(Object.values(props))
-
+const GoalsHome = ({initialGoals, initialStats}) => {
+  const [goals, setGoals] = useState(JSON.parse(initialGoals))
+  const [stats, setStats] = useState(JSON.parse(initialStats))
+  console.log(stats)
   const refreshGoals = async () => {
     const newGoals = await Index()
     if (newGoals.length > 0) {
@@ -20,7 +21,7 @@ const GoalsHome = (props) => {
         <button onClick={refreshGoals}>test</button>
         <h1 className={"text-3xl mb-3"}>Current Goals</h1>
         <hr />
-        {goals.length > 0 ? <GoalsList goals={goals}/>: <h2 className={"text-xl mt-2 text-center "}>No Goals Yet</h2>}
+        {goals.length > 0 ? <GoalsList goals={goals} stats={stats}/>: <h2 className={"text-xl mt-2 text-center "}>No Goals Yet</h2>}
       </div>
       <div className={"p-6 m-2 mx-auto bg-white rounded-xl shadow-lg shrink-0 basis-1/3"}>
         <h1 className={"text-3xl mb-3"}> Create New Goal </h1>
@@ -37,19 +38,8 @@ const GoalsHome = (props) => {
 };
 
 GoalsHome.propTypes = {
-    props: PropTypes.objectOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            title: PropTypes.string.isRequired,
-            deadline: PropTypes.string,
-            comparator: PropTypes.string.isRequired,
-            target_value: PropTypes.string.isRequired,
-            target_metric: PropTypes.string.isRequired,
-            interval: PropTypes.string.isRequired,
-            created_at: PropTypes.string.isRequired,
-            updated_at: PropTypes.string.isRequired
-        })
-    )
-};
+    initialGoals: PropTypes.string,
+    initialStats: PropTypes.string
+}
 
 export default GoalsHome;
--- app/javascript/bundles/Resolutions/components/GoalsList.jsx
+++ app/javascript/bundles/Resolutions/components/GoalsList.jsx
@@ -5,17 +5,27 @@ import GoalsHome from "./GoalsHome";
 const humanize = (str) => {
   return str.replace(/_/g," ")
 }
-const GoalsList = ({goals}) => {
+const GoalsList = ({goals, stats}) => {
+  const statsDict = {}
+  stats.map((stat)=> statsDict[stat.goal_id] = stat)
+
+  const calculateProgress = (stat, target, comparator) => {
+    if (comparator == 'less_than'){
+      return (Number(target)/Number(stat))
+    }
+    return stat/target
+  }
+
   return(
     goals.map((goal) => (
-      <div key={goal.id}  className={"p-1 border rounded-lg w-full mt-2"}>
+      <div className={"p-1 border rounded-lg w-full mt-2"}>
         <h2 className={"text-2xl"}>{goal.title}</h2>
         <h3 className={"text-xl"}> SMART goal: </h3>
         <p>Achieve {humanize(goal.comparator)} {goal.target_value} {goal.target_metric} {goal.interval}</p>
         <h3 className={"text-xl"}> Most recent stat: </h3>
-        <p> 2000 steps on 3/14</p>
+        <p>{statsDict[goal.id] ? `${statsDict[goal.id].activity_value} ${goal.target_metric} on ${statsDict[goal.id].activity_at} UTC` : 'No Stats Logged Yet'}</p>
         <h3 className={"text-xl"}> Progress </h3>
-        <progress className={"w-full"} value={0.5}/>
+        <progress className={"w-full"} value={statsDict[goal.id] ? calculateProgress(statsDict[goal.id].activity_value, goal.target_value, goal.comparator) : 0}/>
       </div>
     ))
   )
@@ -28,7 +38,7 @@ GoalsList.propTypes = {
       title: PropTypes.string.isRequired,
       deadline: PropTypes.string,
       comparator: PropTypes.string.isRequired,
-      target_value: PropTypes.string.isRequired,
+      target_value: PropTypes.number.isRequired,
       target_metric: PropTypes.string.isRequired,
       interval: PropTypes.string.isRequired,
       created_at: PropTypes.string.isRequired,
