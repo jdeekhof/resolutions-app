9db50cad18188cc2a4d75b1fe7838a3885072069
Jake <jdeekhof@gmail.com>
2024-03-25 13:12:00 -0400

    create goals form

--- app/javascript/bundles/Resolutions/components/CreateGoalForm.jsx
+++ app/javascript/bundles/Resolutions/components/CreateGoalForm.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { useForm } from "react-hook-form";
+import PropTypes from "prop-types";
 
-const CreateGoalForm = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+const CreateGoalForm = ({refreshGoals, createGoal}) => {
+  const { register, handleSubmit, reset ,formState: { errors } } = useForm();
 
-  const onSubmit = data => {
-    alert(`Form submitted with name: ${Object.values(data)}`);
+  const onSubmit = async (data) => {
+    const response = await createGoal(data)
+    refreshGoals()
+    reset()
   };
 
   return (
@@ -14,57 +17,63 @@ const CreateGoalForm = () => {
         <div className="col-span-full mt-2">
           <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900">Describe Goal</label>
           <div className="mt-2">
-            <textarea {...register('title', { required: 'Title is required' })} className="block w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-1.5"></textarea>
-            {errors.title && <span className={'text-rose-800 mx-auto'}>{errors.title.message}</span>}
+            <textarea id="title" {...register("title", { required: "Title is required" })} className="block w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-1.5"></textarea>
+            {errors.title && <span className={"text-rose-800 mx-auto"}>{errors.title.message}</span>}
           </div>
         </div>
         <div className="col-span-full mt-2">
-          <label htmlFor="comparator" className="block text-sm  min-h-9 font-medium leading-6 text-gray-900">I want to (spend, walk, eat, etc.) </label>
-          <select {...register('comparator', { required: 'Comparator is required' })} className="block w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-1.5">
-            <option key='0'>Less Than</option>
-            <option key='1'>More Than</option>
+          <label htmlFor="comparator" className="block text-sm  min-h-9 font-medium leading-6 text-gray-900">I want to (spend, walk, eat, etc.)</label>
+          <select id="comparator" {...register("comparator", { required: "Comparator is required" })} className="block w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-1.5">
+            <option value="0">Less Than</option>
+            <option value="1">More Than</option>
           </select>
-          {errors.comparator && <span className={'text-rose-800 mx-auto'}>{errors.comparator.message}</span>}
+          {errors.comparator && <span className={"text-rose-800 mx-auto"}>{errors.comparator.message}</span>}
         </div>
-        <div className={'flex flex-row gap-1'}>
+        <div className={"flex flex-row gap-1"}>
           <div className="mt-2 basis-1/3">
             <input
               type="number"
               placeholder={1}
-              {...register('targetValue',
-                { required: 'Value is required',
+              {...register("targetValue",
+                { required: "Value is required",
                   valueAsNumber: true,
-                  min: { value: 1, message: 'must be atleast one' }
+                  min: { value: 1, message: "must be atleast one" }
                 })
               }
               className="block w-full min-h-9 rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm leading-6 mx-auto p-1.5"></input>
-            {errors.targetValue && <span className={'text-rose-800 mx-auto'}>{errors.targetValue.message}</span>}
+            {errors.targetValue && <span className={"text-rose-800 mx-auto"}>{errors.targetValue.message}</span>}
           </div>
           <div className="mt-2 basis-2/3">
             <input
-              placeholder='steps, pages, etc.'
-              {...register('targetMetric',
-                { required: 'Metric is required'})
+              placeholder="steps, pages, etc."
+              {...register("targetMetric",
+                { required: "Metric is required"})
               }
               className="block w-full min-h-9 rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm leading-6 mx-auto p-1.5"></input>
-            {errors.targetMetric && <span className={'text-rose-800 mx-auto'}>{errors.targetMetric.message}</span>}
+            {errors.targetMetric && <span className={"text-rose-800 mx-auto"}>{errors.targetMetric.message}</span>}
           </div>
         </div>
         <div className="col-span-full mt-2">
           <label htmlFor="interval" className="block text-sm font-medium leading-6 text-gray-900">Within this interval</label>
           <div className="mt-2">
             <input
-              placeholder='within a week, per day, in under 30 minutes'
-              {...register('interval', { required: 'Interval is required' })}
+              id="interval"
+              placeholder="within a week, per day, in under 30 minutes"
+              {...register("interval", { required: "Interval is required" })}
               className="block w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-1.5">
             </input>
-            {errors.interval && <span className={'text-rose-800 mx-auto'}>{errors.interval.message}</span>}
+            {errors.interval && <span className={"text-rose-800 mx-auto"}>{errors.interval.message}</span>}
           </div>
         </div>
         <button className="mt-4 w-full px-4 py-2 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm shadow-sm hover:shadow-md transition duration-300 ease-in-out" type="submit">Submit</button>
       </div>
     </form>
   );
+}
+
+CreateGoalForm.prototype = {
+  refreshGoals: PropTypes.func.isRequired,
+  createGoal: PropTypes.func.isRequired
 }
 
 export default CreateGoalForm;
---
+++ spec/javascript/Resolutions/Components/CreateGoalsForm.spec.js
@@ -0,0 +1,58 @@
+import React from "react"
+import "@testing-library/jest-dom"
+import { render, screen, waitFor } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import CreateGoalForm from "../../../../app/javascript/bundles/Resolutions/Components/CreateGoalForm"
+
+describe("CreateGoalForm", () => {
+  const mockRefreshGoals = jest.fn();
+  const mockCreateGoal = jest.fn().mockResolvedValue({ message: "Goal created successfully" });
+  const validInputs = async () => {
+    const titleInput = screen.getByLabelText("Describe Goal");
+    const comparatorSelect = screen.getByLabelText("I want to (spend, walk, eat, etc.)");
+    const targetValueInput = screen.getByPlaceholderText(1);
+    const targetMetricInput = screen.getByPlaceholderText("steps, pages, etc.");
+    const intervalInput = screen.getByLabelText("Within this interval")
+
+    await userEvent.type(titleInput, "Read more books");
+    userEvent.selectOptions(comparatorSelect, "More Than");
+    await userEvent.type(targetValueInput, "20");
+    await userEvent.type(targetMetricInput, "pages");
+    await userEvent.type(intervalInput, "per day");
+    userEvent.click(screen.getByRole("button", { name: /submit/i }));
+  }
+  beforeEach(() => {
+    window.alert = jest.fn();
+    jest.clearAllMocks();
+    render(<CreateGoalForm refreshGoals={mockRefreshGoals} createGoal={mockCreateGoal} />);
+  });
+
+  it("displays validation errors when required fields are missing", async () => {
+    const submitButton = screen.getByRole("button", { name: /submit/i });
+    userEvent.click(submitButton);
+
+    // Wait for any asynchronous actions to complete
+    await waitFor(() => {
+      expect(screen.getByText("Title is required")).toBeInTheDocument();
+      expect(screen.getByText("Value is required")).toBeInTheDocument();
+      expect(screen.getByText("Metric is required")).toBeInTheDocument();
+      expect(screen.getByText("Interval is required")).toBeInTheDocument();
+    });
+
+    expect(mockCreateGoal).not.toHaveBeenCalled();
+  });
+
+  it("submits the form and calls createGoal with correct data", async () => {
+    validInputs()
+    await waitFor(() => {
+      expect(mockCreateGoal).toHaveBeenCalledWith({
+        title: "Read more books",
+        comparator: "1",
+        targetValue: 20,
+        targetMetric: "pages",
+        interval: "per day"
+      });
+    });
+    expect(mockRefreshGoals).toHaveBeenCalled();
+  });
+});
