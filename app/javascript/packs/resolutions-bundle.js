56cfd93f1facdf59e5499729945190f2f9ae2573
Jake <jdeekhof@gmail.com>
2024-03-26 13:34:34 -0400

    heroku stuff

--- app/javascript/packs/resolutions-bundle.js
+++ app/javascript/packs/resolutions-bundle.js
@@ -1,6 +1,6 @@
 import ReactOnRails from "react-on-rails";
 
-import GoalsHome from "../bundles/./Resolutions/./Components/GoalsHome";
+import GoalsHome from "../components/GoalsHome";
 
 // This is how react_on_rails can see the GoalsHome in the browser.
 ReactOnRails.register({
